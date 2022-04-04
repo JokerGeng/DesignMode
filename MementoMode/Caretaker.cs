91e9bfb1ea7c7367e7ca877b5240400614262137
Gst <leegxf@gmail.com>
2022-04-04 23:00:33 +0800

    备忘录模式

---
+++ MementoMode/Caretaker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MementoMode
+{
+    // The Caretaker doesn't depend on the Concrete Memento class. Therefore, it
+    // doesn't have access to the originator's state, stored inside the memento.
+    // It works with all mementos via the base Memento interface.
+    public class Caretaker
+    {
+        private List<IMemento> _mementos = new List<IMemento>();
+
+        private Originator _originator = null;
+
+        public Caretaker(Originator originator)
+        {
+            this._originator = originator;
+        }
+
+        //向原发器请求当前状态信息,
+        //利用描述的信息初始化一个备忘录
+        public void Backup()
+        {
+            Console.WriteLine("\nCaretaker: Saving Originator's state...");
+            this._mementos.Add(this._originator.Save());
+            Console.WriteLine();
+        }
+
+        //利用备忘录存储的信息恢复原发器的状态
+        public void Undo()
+        {
+            if (_mementos.Count==0)
+            {
+                return;
+            }
+
+            var memento = _mementos.Last();
+            _mementos.Remove(memento);
+
+            Console.WriteLine("Caretaker: Restoring state to: " + memento.GetName());
+
+            try
+            {
+                this._originator.Restore(memento);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Caretaker: Restoring state exception");
+            }
+
+        }
+
+        public void ShowHistory()
+        {
+            Console.WriteLine("Caretaker: Here's the list of mementos:");
+
+            foreach (var memento in this._mementos)
+            {
+                Console.WriteLine(memento.GetName());
+            }
+        }
+    }
+}
---
+++ MementoMode/Originator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MementoMode
+{
+    public class Originator
+    {
+        private string _state;
+
+        public Originator(string state)
+        {
+            _state = state;
+            Console.WriteLine("Originator: My initial state is: " + state);
+        }
+
+        //创建一个备忘录,记录当前时刻的状态
+        public IMemento Save()
+        {
+            return new ConcreteMemento(_state);
+        }
+
+        //使用备忘录恢复内部状态
+        public void Restore(IMemento memento)
+        {
+            if(!(memento is ConcreteMemento))
+            {
+                Console.WriteLine("Unknown memento class " + memento.ToString());
+            }
+            else
+            {
+                _state = memento.GetState();
+                Console.Write($"Originator: My state has changed to: {_state}");
+            }
+        }
+
+        //做某些事情改变了本身的状态
+        private string GenerateRandomString(int length = 10)
+        {
+            string allowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string result = string.Empty;
+
+            while (length > 0)
+            {
+                result += allowedSymbols[new Random().Next(0, allowedSymbols.Length)];
+
+                Thread.Sleep(12);
+
+                length--;
+            }
+
+            return result;
+        }
+
+        // The Originator's business logic may affect its internal state.
+        // Therefore, the client should backup the state before launching
+        // methods of the business logic via the save() method.
+        public void DoSomething()
+        {
+            Console.WriteLine("Originator: I'm doing something important.");
+            this._state = this.GenerateRandomString(30);
+            Console.WriteLine($"Originator: and my state has changed to: {_state}");
+        }
+    }
+}
