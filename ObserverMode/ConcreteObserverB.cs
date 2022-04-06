c4cf50fe3f597d3b6addb060f89433b5c07e64c9
Gst <leegxf@gmail.com>
2022-04-06 22:28:36 +0800

    观察者

---
+++ ObserverMode/ConcreteObserverB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverMode
+{
+    public class ConcreteObserverB : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if ((subject as Subject).State < 3)
+            {
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+            }
+        }
+    }
+}
---
+++ ObserverMode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ObserverMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
+
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            subject.SomeBusinessLogic();
+            Console.Read();
+        }
+    }
+}
