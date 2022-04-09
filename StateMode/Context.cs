e0da1e0674c5b6758cb257136a36c887f32f889f
Gst <leegxf@gmail.com>
2022-04-09 11:02:50 +0800

    状态模式

---
+++ StateMode/Context.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateMode
+{
+    public class Context
+    {
+        private State _state;
+
+        public Context()
+        {
+
+        }
+
+        public Context(State state)
+        {
+            _state = state;
+            _state.SetContext(this); 
+            Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
+        }
+
+        public void SetState(State state)
+        {
+            _state = state;
+            _state.SetContext(this);
+            Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
+        }
+
+        // The Context delegates part of its behavior to the current State
+        // object.
+        public void Request1()
+        {
+            this._state.Handle1();
+        }
+
+        public void Request2()
+        {
+            this._state.Handle2();
+        }
+    }
+}
---
+++ StateMode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StateMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+            ConcreteStateA stateA = new ConcreteStateA();
+
+
+            context.SetState(stateA);
+            context.Request1();
+            context.Request2();
+
+            Console.Read();
+        }
+    }
+}
